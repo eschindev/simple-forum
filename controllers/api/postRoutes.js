6e60b0822c00a713ed7aef4fcb2ef3f8b9af80db
eschindev <118208710+eschindev@users.noreply.github.com>
2023-05-06 16:13:57 -0700

    added delete route for posts, update and delete routes for replies

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -44,3 +44,31 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let postData = await Post.findByPk(req.params.id);
+
+    if (req.session.user_id !== postData.user_id) {
+      return res
+        .status(400)
+        .json({ message: "Failed to delete post: this is not your post." });
+    }
+
+    postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!postData) {
+      return res
+        .status(400)
+        .json({ message: "Failed to delete post. Please try again." });
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
---
+++ controllers/api/replyRoutes.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const { User, Reply } = require("../../models");
+
+router.post("/", async (req, res) => {
+  try {
+    const replyData = await Reply.create(req.body);
+
+    if (!replyData) {
+      return res
+        .status(400)
+        .json({ message: "Failed to create reply. Please try again." });
+    }
+
+    res.status(200).json(replyData);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    let replyData = await Reply.findByPk(req.params.id);
+
+    if (req.session.user_id !== replyData.user_id) {
+      return res
+        .status(400)
+        .json({ message: "Failed to update reply: this is not your reply." });
+    }
+
+    replyData = await Reply.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!replyData) {
+      return res
+        .status(400)
+        .json({ message: "Failed to update reply. Please try again." });
+    }
+
+    res.status(200).json(replyData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let replyData = await Reply.findByPk(req.params.id);
+
+    if (req.session.user_id !== replyData.user_id) {
+      return res
+        .status(400)
+        .json({ message: "Failed to delete reply: this is not your reply." });
+    }
+
+    replyData = await Reply.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!replyData) {
+      return res
+        .status(400)
+        .json({ message: "Failed to delete reply. Please try again." });
+    }
+
+    res.status(200).json(replyData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
