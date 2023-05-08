49ee84b3af0206f3ac6beeed4f46a0a06b6e8e91
eschindev <118208710+eschindev@users.noreply.github.com>
2023-05-08 10:04:56 -0700

    fixed display of post creator on homepage

--- controllers/html/homeRoutes.js
+++ controllers/html/homeRoutes.js
@@ -6,10 +6,11 @@ router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
       order: [["created_on", "DESC"]],
+      include: { model: User },
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
-
+    console.log(posts);
     res.render("homepage", {
       posts,
       logged_in: req.session.logged_in,
